5cff8a6814d10183e4ee83a3f561e5e0581529a9
Loïc Fejoz <loic@fejoz.net>
2017-05-31 14:15:23 +0200

    chore(Trace): Add copyright header in Html Trace exporter project.

--- Time4Sys/plugins/org.polarsys.time4sys.trace.html/src/org/polarsys/time4sys/trace/html/popup/actions/ExportAsHtml.java
+++ Time4Sys/plugins/org.polarsys.time4sys.trace.html/src/org/polarsys/time4sys/trace/html/popup/actions/ExportAsHtml.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2017 RealTime-at-Work and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Loïc Fejoz - initial API and implementation
+ *******************************************************************************/
 package org.polarsys.time4sys.trace.html.popup.actions;
 
 import java.io.File;
