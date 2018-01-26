3b1a697e96f0d5d8461cbcdf81ff08a527dd5ed5
Aurelien Didier <aurelien.didier51@gmail.com>
2018-01-26 18:02:51 +0100

    Add pom for exectime plugin and wip on validation.
    Signed-off-by: Aurelien Didier <aurelien.didier51@gmail.com>

---
+++ Time4Sys/plugins/org.polarsys.time4sys.analysis.tools/src/org/polarsys/time4sys/analysis/tools/AbstractTime4SysRule.java
@@ -0,0 +1,9 @@
+package org.polarsys.time4sys.analysis.tools;
+
+import org.eclipse.emf.validation.AbstractModelConstraint;
+
+public abstract class AbstractTime4SysRule extends AbstractModelConstraint {
+
+	public boolean isActive = false;
+	
+}
